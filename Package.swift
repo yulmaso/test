dbb1debd1fde4a413e570e9da170f6e2d9f817c4
Plotnikov Aleksey <plotnik97@mail.ru>
2024-06-12 16:16:34 +0200

    Add files via upload

---
+++ Package.swift
@@ -0,0 +1,25 @@
+//
+//  Package.swift
+//  iosApp
+//
+//  Created by Aleksei Plotnikov on 12.06.2024.
+//  Copyright © 2024 orgName. All rights reserved.
+//
+
+import PackageDescription
+
+let package = Package(
+   name: "Shared",
+   platforms: [
+     .iOS(.v14),
+   ],
+   products: [
+      .library(name: "Shared", targets: ["Shared"])
+   ],
+   targets: [
+      .binaryTarget(
+         name: "Shared",
+         url: "https://drive.google.com/file/d/1iB05ALpUw6ZyuvFJvzLr3kyx7BjCUZV4/view?usp=drive_link",
+         checksum:"726a76fdc4a0085f878aa45b07b941bdd2421e5ababa9ca8a1e2c9aadaf20859")
+   ]
+)
